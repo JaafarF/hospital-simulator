ec882f0de8da17e8207d22ce45a6e16fdfe8c735
Jaafar Fora <jaafarfora@gmail.com>
2025-03-16 15:04:12 +0100

    Fix imports, remove import */all

--- src/main/java/com/jaafarfora/hospital_simulator/Main.java
+++ src/main/java/com/jaafarfora/hospital_simulator/Main.java
@@ -1,6 +1,10 @@
 package com.jaafarfora.hospital_simulator;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Arrays;
 
 public class Main {
 
