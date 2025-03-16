4c35798b5b9e94911891628d89d40c813496a968
Jaafar Fora <jaafarfora@gmail.com>
2025-03-17 00:25:30 +0100

    Fix points: Access modifiers, Magic number, improvement of code

--- src/main/java/com/jaafarfora/hospital_simulator/Main.java
+++ src/main/java/com/jaafarfora/hospital_simulator/Main.java
@@ -1,30 +1,26 @@
 package com.jaafarfora.hospital_simulator;
 
-import com.jaafarfora.hospital_simulator.enums.Drug;
 import com.jaafarfora.hospital_simulator.enums.State;
 
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Arrays;
 
 public class Main {
-
-    static List<String> statesInput = new ArrayList<>();
-    static List<String> drugsInput = new ArrayList<>();
-    static Map<State, Integer> statesResult = new LinkedHashMap<>();
+    private static final short MINIMUM_EXPECTED_AMOUNT_OF_ARGUMENTS = 1;
+    private static final short MAXIMUM_EXPECTED_AMOUNT_OF_ARGUMENTS = 2;
+    private static List<String> statesInput = new ArrayList<>();
+    private static List<String> drugsInput = new ArrayList<>();
 
     public static void main(String[] args) {
-
         extractStatesAndDrugs(args);
         InputsValidator.validate(statesInput, drugsInput);
-        statesResult = HealthStateCalculator.calculateState(statesInput, drugsInput);
+        Map<State, Integer> statesResult = HealthStateCalculator.calculateState(statesInput, drugsInput);
         displayResult(statesResult);
     }
 
     private static void extractStatesAndDrugs(String[] args) {
-        if (args.length != 1 && args.length != 2) {
+        if (args.length != MINIMUM_EXPECTED_AMOUNT_OF_ARGUMENTS && args.length != MAXIMUM_EXPECTED_AMOUNT_OF_ARGUMENTS) {
             throw new RuntimeException("You provided an invalid number of arguments, 1 or 2 parts are required");
         }
         statesInput = List.of(args[0].split(","));
